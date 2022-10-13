a91d4e6e536322bb8d98fe739fcb187b0fda274f
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-12 21:20:29 -0400

    -- Fixed issue with queryParam subscription. -- Added the queryParam preservation.

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -21,7 +21,7 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.route.params
+    this.route.queryParams
       .subscribe(
         (queryParams: Params) => {
           this.allowEdit = queryParams['allowEdit'] === '1';
