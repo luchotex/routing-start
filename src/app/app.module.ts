0d8889b4ca85d53dd6bcfc0c2c726a0f0f801975
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-12 21:31:43 -0400

    -- Added page not found and their path with wildcard to cover all the routes that the app doesn't redirect (used redirect to use specific path).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import {UserComponent} from './users/user/user.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -26,7 +27,9 @@ const appRoutes: Routes = [
       {path: ':id/edit', component: EditServerComponent},
       {path: ':id', component: ServerComponent}
     ]
-  }
+  },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: '/not-found'}
 ];
 
 @NgModule({
@@ -37,7 +40,8 @@ const appRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
