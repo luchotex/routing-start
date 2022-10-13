0dba6275793b20f56fb3c24af5b273cf85f78797
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-12 21:13:50 -0400

    -- Added child routes. -- Subscribe query param to retrieve the allowEdit. -- Added button to edit server. -- Added functionality to send to edit server.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,14 +15,19 @@ import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 
 const appRoutes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'users', component: UsersComponent},
-    {path: 'users/:id/:name', component: UserComponent},
-    {path: 'servers', component: ServersComponent},
-    {path: 'servers/:id/edit', component: EditServerComponent},
-    {path: 'servers/:id', component: ServerComponent}
-  ]
-;
+  {path: '', component: HomeComponent},
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent}
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      {path: ':id/edit', component: EditServerComponent},
+      {path: ':id', component: ServerComponent}
+    ]
+  }
+];
 
 @NgModule({
   declarations: [
