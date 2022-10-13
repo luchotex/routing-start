33162958e43da4643d0857d7e07d9e6a7c7f7288
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-12 21:53:30 -0400

    -- Outsourcing routes to another route file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {FormsModule} from '@angular/forms';
 
 
 import {AppComponent} from './app.component';
-import {RouterModule, Routes} from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
 import {UsersComponent} from './users/users.component';
@@ -14,23 +13,8 @@ import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {ServerComponent} from './servers/server/server.component';
 import {ServersService} from './servers/servers.service';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AppRoutingModule} from './app-routing.module';
 
-const appRoutes: Routes = [
-  {path: '', component: HomeComponent},
-  {
-    path: 'users', component: UsersComponent, children: [
-      {path: ':id/:name', component: UserComponent}
-    ]
-  },
-  {
-    path: 'servers', component: ServersComponent, children: [
-      {path: ':id/edit', component: EditServerComponent},
-      {path: ':id', component: ServerComponent}
-    ]
-  },
-  {path: 'not-found', component: PageNotFoundComponent},
-  {path: '**', redirectTo: '/not-found'}
-];
 
 @NgModule({
   declarations: [
@@ -46,7 +30,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
